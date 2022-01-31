8c176b021ca8d6e0f6ea483439ee58f0114fba05
Nadia Sultana <nadiasultana8899@gmail.com>
2022-01-31 18:45:52 -0500

    dessert delight website

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,51 +2,15 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
-            <section>
-                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src="images/dessert.jpeg" className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src="images/images.jpeg" className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src="images/dessert3.jpeg" className="d-block w-100" alt="..." />
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </section>
-            <section>
-                <div className="second-section">
-                    <div className="col-md-4 mt-5">
-                        <img src="images/dessert2.jpeg" className="img-fluid rounded-start" alt="..." />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                                content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-                </div>
-            </section >
+        <div className="body-back">
+             <div class="wrapper">
 
-
-
-
+        <div class="welcome-text">
+            <marquee className><h1>Welcome To Desert Delights</h1></marquee>
+        </div>
+    
         </div >
+        </div>
     )
 }
 
